de86dd75ed805274c95cd19e3a7342799e752dd2
yihengx <yihengx66@gmail.com>
2023-05-10 02:00:40 -0700

    add black and isort as dev dep; run black and isort

--- sphinx_api_sidebar/sphinx_api_sidebar.py
+++ sphinx_api_sidebar/sphinx_api_sidebar.py
@@ -1,9 +1,8 @@
 import os
-import glob
 import shutil
 import subprocess
-
 from pathlib import Path
+
 from sphinx.util import logging
 
 logger = logging.getLogger(__name__)
@@ -18,10 +17,11 @@ TEMPLATE_CONTENT = """{% if api_docs %}
 {% endif %}
 """
 
+
 def write_template_file(app):
-    templates_dir = os.path.join(app.srcdir, '_templates/sidebar')
-    template_path = os.path.isfile(os.path.join(templates_dir, 'api_docs_sidebar.html'))
-    
+    templates_dir = os.path.join(app.srcdir, "_templates/sidebar")
+    template_path = os.path.isfile(os.path.join(templates_dir, "api_docs_sidebar.html"))
+
     # create the directory if it doesn't exist
     os.makedirs(templates_dir, exist_ok=True)
 
@@ -30,22 +30,21 @@ def write_template_file(app):
         return
 
     # else write the template content to api_docs_sidebar.html
-    with open(os.path.join(templates_dir, 'api_docs_sidebar.html'), 'w') as f:
+    with open(os.path.join(templates_dir, "api_docs_sidebar.html"), "w") as f:
         f.write(TEMPLATE_CONTENT)
+
 
 def update_html_context(config, api_docs=[]):
     # update html_context with api_docs
-    config.html_context.update({
-        "api_docs": api_docs
-    })
+    config.html_context.update({"api_docs": api_docs})
+
 
 def generate_api_sidebar(app, config):
-
     # write the template file
     write_template_file(app)
 
     # get the path to the _static/api-docs directory
-    api_docs_dir = os.path.join(app.srcdir, '_static/api-docs')
+    api_docs_dir = os.path.join(app.srcdir, "_static/api-docs")
 
     # delete the directory if it exists
     if os.path.exists(api_docs_dir):
@@ -56,17 +55,16 @@ def generate_api_sidebar(app, config):
     # iterate through the list of dictionaries and run the customized command
     for api_docs_generator in config.api_docs_generators:
         # get the build command from conf.py and run it
-        command = api_docs_generator['command']
+        command = api_docs_generator["command"]
 
-        subprocess.run([f'{command}'], text=True, shell=True, capture_output=True)
+        subprocess.run([f"{command}"], text=True, shell=True, capture_output=True)
 
         # iterate through the list of dictionaries and copy the generated API docs to the static/api-docs directory
-        for output in api_docs_generator['outputs']:
+        for output in api_docs_generator["outputs"]:
+            api_doc_name = output["name"]
 
-            api_doc_name = output['name']
+            output_path = output["path"]
 
-            output_path = output['path']
-            
             shutil.copytree(output_path, os.path.join(api_docs_dir, api_doc_name))
 
             api_docs.append(api_doc_name)
@@ -76,6 +74,6 @@ def generate_api_sidebar(app, config):
 
 
 def setup(app):
-    app.add_config_value('api_docs_generators', [], 'env')
+    app.add_config_value("api_docs_generators", [], "env")
 
-    app.connect('config-inited', generate_api_sidebar)
+    app.connect("config-inited", generate_api_sidebar)
--- tests/test_sphinx_api_sidebar.py
+++ tests/test_sphinx_api_sidebar.py
@@ -1,10 +1,11 @@
 import unittest
+
 from sphinx_api_sidebar.sphinx_api_sidebar import update_html_context
 
-class TestSphinxApiSidebar(unittest.TestCase):
 
+class TestSphinxApiSidebar(unittest.TestCase):
     def test_update_html_context(self):
-        config = type('', (), {})()  # Create a simple object to act as the config
+        config = type("", (), {})()  # Create a simple object to act as the config
         config.html_context = {}
 
         api_docs = ["project1", "project2"]
@@ -14,5 +15,6 @@ class TestSphinxApiSidebar(unittest.TestCase):
         self.assertIn("api_docs", config.html_context)
         self.assertEqual(api_docs, config.html_context["api_docs"])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
